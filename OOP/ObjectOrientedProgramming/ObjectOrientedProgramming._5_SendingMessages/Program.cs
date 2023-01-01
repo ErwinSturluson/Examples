48c8092d5449a461f9a3334b1912e6a0e74028b3
ErwinSturluson <angelmercuries@gmail.com>
2023-01-02 03:10:29 +0600

    add 'Sending Messages' example

--- OOP/ObjectOrientedProgramming/ObjectOrientedProgramming._5_SendingMessages/Program.cs
+++ OOP/ObjectOrientedProgramming/ObjectOrientedProgramming._5_SendingMessages/Program.cs
@@ -1,9 +1,21 @@
 ﻿namespace ObjectOrientedProgramming._5_SendingMessages
 {
+    internal class ClassDemo
+    {
+        public void MethodDemo()
+        {
+        }
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            ClassDemo class1 = new();
+            // Sending the message is named 'MethodDemo' from object of class
+            // 'Program' to object of class 'ClassDemo' even without any arguments
+            class1.MethodDemo();
+
             Console.WriteLine("Hello, World!");
         }
     }
