214bdd34ff31ba4ee120246d9a271c31bfc82231
ErwinSturluson <angelmercuries@gmail.com>
2023-01-01 23:59:46 +0600

    add 'Inheritance' example

--- OOP/ObjectOrientedProgramming/ObjectOrientedProgramming._2_Inheritance/Program.cs
+++ OOP/ObjectOrientedProgramming/ObjectOrientedProgramming._2_Inheritance/Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace ObjectOrientedProgramming._2_Inheritance
 {
+    public class BaseClass
+    {
+        public void BaseClassPublicMethod()
+        {
+        }
+
+        protected void BaseClassProtectedMethod()
+        {
+        }
+
+        private void BaseClassPrivateMethod()
+        {
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public void DerivedClassPublicMethod()
+        {
+            // Error: private methods are not inherited:
+            BaseClassPrivateMethod();
+
+            // Protected and public methods are inherited, but only available
+            // inside the derived classes:
+            BaseClassProtectedMethod();
+        }
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BaseClass baseClass = new();
+            baseClass.BaseClassPublicMethod();
+
+            DerivedClass derivedClass = new();
+            derivedClass.DerivedClassPublicMethod();
+
+            // Public methods are inherited and available outside the derived classes:
+            derivedClass.BaseClassPublicMethod();
+
+            Console.WriteLine("Hello, Inheritance!");
         }
     }
 }
