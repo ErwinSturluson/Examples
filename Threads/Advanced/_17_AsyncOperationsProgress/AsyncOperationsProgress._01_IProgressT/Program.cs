4e15086e3235063b4f6a2f5aa06f2c05da07cb39
ErwinSturluson <angelmercuries@gmail.com>
2023-01-30 18:13:52 +0600

    add Threads Advanced 17 Progress

---
+++ Threads/Advanced/_17_AsyncOperationsProgress/AsyncOperationsProgress._01_IProgressT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncOperationsProgress.IProgressT
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            IProgress<string> progress = new Progress<string>((report) => Console.WriteLine($"[Progress]: {report}"));
+
+            await Task.Run(() => PrintIterations("AsyncTask", progress));
+        }
+
+        private static void PrintIterations(string callName, IProgress<string> progress)
+        {
+            Console.WriteLine($"+++{callName,-12}- Task#{Task.CurrentId,-1} - Thread#{Environment.CurrentManagedThreadId,-1} - Started:[{nameof(PrintIterations)}]");
+
+            int iterationIndex = 0;
+
+            while (iterationIndex < 10)
+            {
+                iterationIndex++;
+
+                progress.Report($"{callName,-12}- Task#{Task.CurrentId,-1}- Task#{Task.CurrentId,-1} - Thread#{Environment.CurrentManagedThreadId,-1} - [{iterationIndex}]");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine($"---{callName,-12}- Task#{Task.CurrentId,-1} - Thread#{Environment.CurrentManagedThreadId,-1} - Finished:[{nameof(PrintIterations)}]");
+        }
+    }
+}
